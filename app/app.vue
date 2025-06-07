f68b97c9ef319d5f077bce1f85072e6d15191d9c
warin101 <varinpimda@gmail.com>
2025-06-07 15:11:15 +0700

    check point 2 - setup project v.2

---
+++ app/app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <div class="bg-blue-950 justify-around ">
+      <h1 class="text-amber-300 bg-black">Hiii warin!!!</h1>
+    </div>
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,57 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  future: {
+    compatibilityVersion: 4,
+  },
+
+  experimental: {
+    scanPageMeta: 'after-resolve',
+    sharedPrerenderData: false,
+    compileTemplate: true,
+    resetAsyncDataToUndefined: true,
+    templateUtils: true,
+    relativeWatchPaths: true,
+    normalizeComponentNames: false,
+    spaLoadingTemplateLocation: 'within',
+    parseErrorData: false,
+    pendingWhenIdle: true,
+    alwaysRunFetchOnKeyChange: true,
+    defaults: {
+      useAsyncData: {
+        deep: true
+      }
+    }
+  },
+
+  unhead: {
+    renderSSRHeadOptions: {
+      omitLineBreaks: false
+    }
+  },
+
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/google-fonts',
+    '@nuxt/icon',
+    '@nuxt/image'
+  ],
+  
+  googleFonts: {
+    families: {
+      Roboto: true,
+      'Josefin+Sans': true,
+      Lato: [100, 300],
+      Raleway: {
+        wght: [100, 400],
+        ital: [100]
+      },
+      Inter: '200..700',
+      'Crimson Pro': {
+        wght: '200..900',
+        ital: '200..700',
+      }
+    }
+  }
 })
