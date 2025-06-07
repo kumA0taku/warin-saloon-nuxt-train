d0937ab7932c2931d5beb91ecf04f2af9c3150b0
warin101 <varinpimda@gmail.com>
2025-06-07 15:59:20 +0700

    ck p1

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,8 @@
 <template>
-  <div>
+  <div class="font-Montserrat container">
     <div class="bg-blue-950 justify-around ">
       <h1 class="text-amber-300 bg-black">Hiii warin!!!</h1>
+      <h1 class="text-amber-300 bg-black">Warinthorn Pimda</h1>
     </div>
   </div>
 </template>
