b1217d05e09500187015729d9ebea1935adb62de
warin101 <varinpimda@gmail.com>
2025-06-07 14:32:28 +0700

    check point 1 - setup project v.1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-
-  modules: [
-    '@nuxt/image',
-    '@nuxt/icon',
-    '@nuxt/fonts',
-    '@nuxt/ui',
-    '@nuxt/test-utils',
-    '@nuxt/scripts',
-    '@nuxt/eslint',
-    '@nuxt/content'
-  ]
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true }
 })
